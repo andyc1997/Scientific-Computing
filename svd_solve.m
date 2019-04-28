c8225ce35039bffc2ef497ca22f33dc1ce938149
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2019-04-29 02:02:27 +0800

    Update and rename hw5q3.m to svd_solve.m

--- svd_solve.m
+++ svd_solve.m
@@ -1,10 +1,5 @@
-%% part (c)
-sigma = [3, 0, 0; 
-        0, 2, 0;
-        0, 0, 1];
-diag(1./diag(sigma)) % one line code
-
-%% part (d)
+% An attempt to use singular value decomposition to solve Ax = b
+% for nonsingular A. 
 A = [1, 1, 0;
     8, 4, 1;
     9, 0, 3];
@@ -20,10 +15,14 @@ b = [1, 0, 1, 2]';
 
 svd_solve(A, b)
 
+% Computational complexity of computing A^T * A: O(n^2.807) ~ O(n^3)
+% Computational complexity of computing eigenvalues & eigenvectors: O(2mn^2) = O(n^3)
+% Gaussian elimination outperforms this method
+
 function x = svd_solve(A, b)
     [V, D] = eig(A'*A); % Compute eigenvalues & eigenvectors of A^T * A
     sigma_inv = diag(1./diag(sqrt(D))); % Sigma inverse
     U = A*V*sigma_inv; % Left matrix
-
+    
     x = V*sigma_inv*U'*b;
 end 
